a9ba7d55fc078c0d65703620ba70ae0915eef68f
Guilherme Isaías <guilherme.silva@ferreri.co>
2023-08-10 20:43:07 -0300

    Tests to run latest command

--- e2e/migration_test.go
+++ e2e/migration_test.go
@@ -107,3 +107,28 @@ func TestDown_WithoutMigrations(t *testing.T) {
 		}
 	})
 }
+
+func TestLatest(t *testing.T) {
+	os.Setenv("MIGRATIONS_DIR", "/usr/src/migrator/e2e/mocks/1")
+	env(func(envs env_set) {
+		t.Log("\nTesting with envs: ", envs, "\n")
+		if err := cli.Run([]string{"migrator", "latest"}); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestLatest_WhenMigrationsWereRan(t *testing.T) {
+	os.Setenv("MIGRATIONS_DIR", "/usr/src/migrator/e2e/mocks/1")
+	env(func(envs env_set) {
+		t.Log("\nTesting with envs: ", envs, "\n")
+		for i := 1; i <= 3; i++ {
+			if err := cli.Run([]string{"migrator", "up"}); err != nil {
+				t.Fatal(err)
+			}
+		}
+		if err := cli.Run([]string{"migrator", "latest"}); err != nil {
+			t.Fatal(err)
+		}
+	})
+}
